6f586010faf015ebc9a53d4c5c46d1733fe05549
kalle.kindvall <kalle.kindvall@dacsa.se>
2026-04-09 11:50:18 +0200

    Add GPT 5.4 game links

--- src/games.js
+++ src/games.js
@@ -9,4 +9,14 @@ export const games = [
     name: 'gemma4e4b-chat-1',
     href: './games/gemma4e4b-chat-1.html',
   },
+  {
+    slug: 'gpt5.4-openclaw-1',
+    name: 'gpt5.4-openclaw-1',
+    href: './games/gpt5.4-openclaw-1.html',
+  },
+  {
+    slug: 'gpt5.4-chat-1',
+    name: 'gpt5.4-chat-1',
+    href: './games/gpt5.4-chat-1.html',
+  },
 ]
